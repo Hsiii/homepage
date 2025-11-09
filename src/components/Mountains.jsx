438721c86d8f801a9cfbb85a39ea2bf4eb9af3b5
陳曦 <eriksonchen@gmail.com>
2025-11-09 15:32:13 +0800

    chore: update image include path

--- src/components/Mountains.jsx
+++ src/components/Mountains.jsx
@@ -5,9 +5,9 @@ import 'components/Mountains.css';
 export default function Mountains() {
     return (
         <div className='mountains'>
-            <img src='images/mountain/back.webp' />
-            <img src='images/mountain/mid.webp' />
-            <img src='images/mountain/front.webp' />
+            <img src='images/mountain/back.svg' />
+            <img src='images/mountain/mid.svg' />
+            <img src='images/mountain/front.svg' />
         </div>
     );
 }
