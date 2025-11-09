bdffd98db15a043131e08d1f1a9f268722eab23b
陳曦 <eriksonchen@gmail.com>
2025-11-09 15:31:52 +0800

    chore: update image credit link

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -12,7 +12,9 @@ export default function Main() {
                 <Cover />
             </main>
             <footer>
-                <a href='http://www.freepik.com'>Cover image designed by pikisuperstar / Freepik</a>
+                <a href='https://www.freepik.com/free-vector/welcome-landing-page-with-gradient-landscape_5060698.htm#fromView=search&page=1&position=22&uuid=29afede1-c055-4f7a-82bc-35b5b30e8d44&query=%40pikisuperstar+landscape'>
+                    Image designed by pikisuperstar on Freepik
+                </a>
             </footer>
         </>
     );
