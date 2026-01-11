fcb8068aa1d5f554913b1667f11cd48a4ce784f5
陳曦 <eriksonchen@gmail.com>
2026-01-11 20:11:13 +0800

    feat: show a placeholder when no mountains have loaded

--- src/components/Mountains.tsx
+++ src/components/Mountains.tsx
@@ -1,19 +1,28 @@
+import { useState } from 'react';
+
 import 'components/Mountains.css';
 
 export default function Mountains() {
+    const [loadedCount, setLoadedCount] = useState(0);
+    const handleLoad = () => setLoadedCount((p) => p + 1);
+
     return (
         <div className='mountains'>
+            <div className={`placeholder ${loadedCount > 0 ? 'hidden' : ''}`} />
             <img
                 src='images/mountain/back.svg'
                 alt='a flat-color mountain in the background'
+                onLoad={handleLoad}
             />
             <img
                 src='images/mountain/mid.svg'
                 alt='a flat-color mountain in the middle'
+                onLoad={handleLoad}
             />
             <img
                 src='images/mountain/front.svg'
                 alt='a flat-color mountain in the foreground'
+                onLoad={handleLoad}
             />
         </div>
     );
