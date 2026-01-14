bc5741a54c13799e6cfa81beae973095fa2cfe88
陳曦 <eriksonchen@gmail.com>
2026-01-14 14:39:15 +0800

    refactor: import all images

--- src/components/Mountains.tsx
+++ src/components/Mountains.tsx
@@ -1,4 +1,6 @@
 import backSrc from '../assets/images/mountain/back.svg';
+import frontSrc from '../assets/images/mountain/front.svg';
+import midSrc from '../assets/images/mountain/mid.svg';
 
 import 'components/Mountains.css';
 
@@ -12,13 +14,10 @@ export default function Mountains() {
             />
             <img
                 className='parallax-mid'
-                src='assets/images/mountain/mid.svg'
+                src={midSrc}
                 alt='a flat-color mountain in the middle'
             />
-            <img
-                src='assets/images/mountain/front.svg'
-                alt='a flat-color mountain in the foreground'
-            />
+            <img src={frontSrc} alt='a flat-color mountain in the foreground' />
         </div>
     );
 }
