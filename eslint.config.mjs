70a81d149bb1864b60648d69b4087575aec81abc
Hsi <94444077+Hsiii@users.noreply.github.com>
2026-01-27 17:01:55 +0800

    chore: migrate to complete-lint (#7)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,68 +1,12 @@
-import { fixupConfigRules, fixupPluginRules } from '@eslint/compat';
-import js from '@eslint/js';
-import importPlugin from 'eslint-plugin-import';
-import pluginReact from 'eslint-plugin-react';
-import pluginReactHooks from 'eslint-plugin-react-hooks';
-import sortKeys from 'eslint-plugin-sort-keys';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import { completeConfigBase } from 'eslint-config-complete';
+import { defineConfig } from 'eslint/config';
 
-export default tseslint.config(
+export default defineConfig(
+    ...completeConfigBase,
+
     {
-        ignores: ['dist/'],
-    },
-    js.configs.recommended,
-    ...tseslint.configs.recommended,
-    ...fixupConfigRules(pluginReact.configs.flat.recommended),
-    {
-        plugins: {
-            'react-hooks': fixupPluginRules(pluginReactHooks),
-        },
         rules: {
-            ...pluginReactHooks.configs.recommended.rules,
-        },
-    },
-    {
-        settings: {
-            react: {
-                version: 'detect',
-            },
-        },
-    },
-    {
-        files: ['**/*.{ts,tsx}'],
-        languageOptions: {
-            parser: tseslint.parser,
-            parserOptions: {
-                project: './tsconfig.json',
-                tsconfigRootDir: import.meta.dirname,
-            },
-            globals: {
-                ...globals.browser,
-                ...globals.node,
-                webpack: 'readonly',
-            },
-        },
-        rules: {
-            'react/react-in-jsx-scope': 'off',
-        },
-    },
-    {
-        plugins: {
-            import: fixupPluginRules(importPlugin),
-        },
-        rules: {
-            'import/no-duplicates': ['error', { considerQueryString: true }],
-            'import/order': 'off',
-        },
-    },
-    {
-        files: ['**/links.ts'],
-        plugins: {
-            'sort-keys': sortKeys,
-        },
-        rules: {
-            'sort-keys/sort-keys-fix': 'error',
+            // Insert changed or disabled rules here, if necessary.
         },
     }
 );
