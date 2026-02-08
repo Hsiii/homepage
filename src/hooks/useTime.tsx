570c1096486a82a2ef789bf184919cde117bcff4
陳曦 <eriksonchen@gmail.com>
2026-02-08 17:28:17 +0800

    perf: use date cleanup

--- src/hooks/useTime.tsx
+++ src/hooks/useTime.tsx
@@ -1,22 +1,28 @@
 import { useEffect, useState } from 'react';
 
-export const useTime = () => {
+interface Time {
+    time: string;
+    date: string;
+}
+
+export const useTime = (): Time => {
     const [time, setTime] = useState('');
     const [date, setDate] = useState('');
 
     useEffect(() => {
-        requestAnimationFrame(() => {
-            const update = () => {
-                const d = new Date();
-                const hours = d.getHours().toString().padStart(2, '0');
-                const minutes = d.getMinutes().toString().padStart(2, '0');
-                setTime(`${hours}:${minutes}`);
-                setDate(d.getMonth() + 1 + '/' + d.getDate());
-            };
-            update();
-            const id = setInterval(update, 1000 * 60);
-            return () => clearInterval(id);
-        });
+        const update = () => {
+            const d = new Date();
+            const hours = d.getHours().toString().padStart(2, '0');
+            const minutes = d.getMinutes().toString().padStart(2, '0');
+            setTime(`${hours}:${minutes}`);
+            setDate(`${d.getMonth() + 1}/${d.getDate()}`);
+        };
+
+        requestAnimationFrame(update);
+        const id = setInterval(update, 1000 * 60);
+        return () => {
+            clearInterval(id);
+        };
     }, []);
 
     return { time, date };
