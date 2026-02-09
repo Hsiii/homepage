83fd0e90b9538b9f84beaa3c4393102f723bfb3f
Hsi <94444077+Hsiii@users.noreply.github.com>
2026-02-09 16:56:36 +0800

    chore: use port 3000 (#35)

--- api/weather.ts
+++ api/weather.ts
@@ -16,10 +16,8 @@ interface WeatherPayload {
 
 const allowedOrigins = [
     'https://hsi-homepage.vercel.app',
-    'http://localhost:4173',
-    'http://localhost:5173',
-    'http://127.0.0.1:4173',
-    'http://127.0.0.1:5173',
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
 ];
 
 const defaultCoords = {
