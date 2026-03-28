280f6e553f72aa3b84abe040b88c9f034990213b
Hsi <its.hsichen@gmail.com>
2026-03-28 12:03:47 +0800

    perf(mobile): lazy-load mobile warning

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,17 +1,31 @@
+import { lazy, Suspense, useState } from 'react';
+
 import { footerCredit, footerLink } from '@/constants/footer';
 import { Cover } from './Cover';
-import { MobileWarning } from './MobileWarning';
 
 import './Main.css';
 
-export const Main: React.FC = () => (
-    <>
-        <MobileWarning />
-        <main>
-            <Cover />
-        </main>
-        <footer>
-            <a href={footerLink}>{footerCredit}</a>
-        </footer>
-    </>
+const MobileWarning = lazy(
+    async () =>
+        await import('./MobileWarning').then((module) => ({
+            default: module.MobileWarning,
+        }))
 );
+
+export const Main: React.FC = () => {
+    const [isMobileViewport] = useState(() => globalThis.innerWidth < 600);
+
+    return (
+        <>
+            <Suspense fallback={undefined}>
+                {isMobileViewport ? <MobileWarning /> : undefined}
+            </Suspense>
+            <main>
+                <Cover />
+            </main>
+            <footer>
+                <a href={footerLink}>{footerCredit}</a>
+            </footer>
+        </>
+    );
+};
