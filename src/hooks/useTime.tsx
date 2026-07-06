e199457c6b35eab8a6f481c03b09782a1a9bbd1a
Hsi <its.hsichen@gmail.com>
2026-07-06 09:11:26 +0800

    fix: render time on initial load

--- src/hooks/useTime.tsx
+++ src/hooks/useTime.tsx
@@ -4,21 +4,21 @@ interface Time {
     time: string;
 }
 
+const getCurrentTime = (): string =>
+    new Date().toLocaleTimeString('en-US', {
+        hour: '2-digit',
+        hour12: false,
+        minute: '2-digit',
+    });
+
 export const useTime = (): Time => {
-    const [time, setTime] = useState('');
+    const [time, setTime] = useState(getCurrentTime);
 
     useEffect(() => {
         const update = () => {
-            setTime(
-                new Date().toLocaleTimeString('en-US', {
-                    hour: '2-digit',
-                    hour12: false,
-                    minute: '2-digit',
-                })
-            );
+            setTime(getCurrentTime());
         };
 
-        requestAnimationFrame(update);
         const id = setInterval(update, 1000 * 60);
         return () => {
             clearInterval(id);
